abce7acc30f04818690507ef349703314a0ae193
bashar125824 <bashar.alrefae.7@gmail.com>
2022-04-28 05:52:28 +0300

    Complete Lab 14

---
+++ MyFirstApp/MyFirstApp/Models/Interfaces/IHotelRoom.cs
@@ -0,0 +1,23 @@
+﻿using MyFirstApp.Data;
+using MyFirstApp.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.Models.Interfaces
+{
+    
+        public interface IHotelRoom
+        {
+        Task<HotelRoom> Create(HotelRoom hotelRoom);
+        Task<List<HotelRoom>> GetHotelRooms();
+        // we need to specify something unique like primary key, but we don't have it 
+        // due to this we will use composite keys to specify entity, to make it unique
+        Task<HotelRoom> GetHotelRoom(int HotelId, int RoomNum);
+        Task<HotelRoom> UpdateHotelRoom(int HotelId, int RoomNum, HotelRoom hotelRoom);
+        Task Delete(int HotelId, int RoomNum);
+        }
+
+}
+    
---
+++ MyFirstApp/MyFirstApp/Models/Services/HotelRoomServices.cs
@@ -0,0 +1,54 @@
+﻿using MyFirstApp.Data;
+using MyFirstApp.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.Models.Services
+{
+    public class HotelRoomServices : IHotelRoom
+    {
+
+        private readonly schoolDbContext _context;
+        public HotelRoomServices(schoolDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<HotelRoom> Create( HotelRoom hr)
+        {
+            _context.Entry(hr).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+            return hr;
+        }
+
+        public async Task<HotelRoom> GetHotelRoom(int HotelId, int RoomNum)
+        {
+            return await _context.HotelRooms.Include(hr => hr.roomId)
+                                            .Include(hr => hr.hotelId)
+                                            .ThenInclude(h => h)
+                                            .FirstOrDefaultAsync(x => x.hotelId == HotelId && x.roomNum == RoomNum); 
+        }
+
+        public async Task<List<HotelRoom>> GetHotelRooms()
+        {
+            return await _context.HotelRooms.Include(hr => hr.roomId)
+                                            .Include(hr => hr.hotelId)
+                                            .ToListAsync(); 
+        }
+
+        public async Task<HotelRoom> UpdateHotelRoom(int HotelId, int RoomNum, HotelRoom hr)
+        {
+            _context.Entry(hr).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return hr;
+        }
+        public async Task Delete(int HotelId, int RoomNum)
+        {
+            HotelRoom hr = await GetHotelRoom(HotelId, RoomNum);
+            _context.Entry(hr).State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
